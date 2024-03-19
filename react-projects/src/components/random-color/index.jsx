f7bf7b4b5a3a47c2c8a04c4a5e000da2d8247fca
Fabrice <nkurunziz@alustudent.com>
2024-03-19 11:35:47 +0200

    created the component for generating random color

--- react-projects/src/components/random-color/index.jsx
+++ react-projects/src/components/random-color/index.jsx
@@ -3,26 +3,71 @@ import "./styles.css";
 
 export default function GenerateRandomColor() {
   const [rgbColor, setRgbColor] = useState({ red: 0, green: 0, blue: 0 });
+  const [hexColor, setHexColor] = useState({ red: 0, green: 0, blue: 0 });
   const [colorForm, setColorForm] = useState("rgb");
 
   function GenerateRandomColor() {
-    setRgbColor((prevRgb) => {
-      const newRGB = { ...prevRgb };
-      newRGB.red = Math.floor(Math.random() * 250);
-      newRGB.green = Math.floor(Math.random() * 250);
-      newRGB.blue = Math.floor(Math.random() * 250);
+    if (colorForm === "rgb") {
+      setRgbColor((prevRgb) => {
+        const newRGB = { ...prevRgb };
+        newRGB.red = Math.floor(Math.random() * 250);
+        newRGB.green = Math.floor(Math.random() * 250);
+        newRGB.blue = Math.floor(Math.random() * 250);
 
-      return newRGB;
-    });
+        return newRGB;
+      });
+    } else {
+      const randomValues = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+      ];
+      setHexColor((prevHEX) => {
+        const newHEX = { ...prevHEX };
+        newHEX.red = `${
+          randomValues[Math.floor(Math.random() * randomValues.length)]
+        }${randomValues[Math.floor(Math.random() * randomValues.length)]}`;
+        newHEX.green = `${
+          randomValues[Math.floor(Math.random() * randomValues.length)]
+        }${randomValues[Math.floor(Math.random() * randomValues.length)]}`;
+        newHEX.blue = `${
+          randomValues[Math.floor(Math.random() * randomValues.length)]
+        }${randomValues[Math.floor(Math.random() * randomValues.length)]}`;
+
+        return newHEX;
+      });
+    }
   }
 
   document.body.style.backgroundColor =
     colorForm === "rgb"
       ? `rgb(${rgbColor.red}, ${rgbColor.green}, ${rgbColor.blue})`
-      : "#00FF00";
+      : `#${hexColor.red}${hexColor.green}${hexColor.blue}`;
   return (
     <div className="container">
-      <button onClick={() => setColorForm("hex")}>Generate HEX Color</button>
+      <button onClick={() => setColorForm("hex")}>
+        Generate HEX Color
+        {colorForm === "hex" && (
+          <span>
+            {" "}
+            #{hexColor.red}
+            {hexColor.green}
+            {hexColor.blue}
+          </span>
+        )}
+      </button>
       <button onClick={() => setColorForm("rgb")}>
         Generate RGB Color
         {colorForm === "rgb" && (
