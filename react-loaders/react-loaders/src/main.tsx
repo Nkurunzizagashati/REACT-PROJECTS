374e4b1e478b251d588a61f609c791008e1ae97d
Fabrice <f.nkurunziz@alustudent.com>
2024-09-24 17:32:36 +0200

    react loader

--- react-loaders/react-loaders/src/main.tsx
+++ react-loaders/react-loaders/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/home',
+		element: <h1>Home Page</h1>,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
